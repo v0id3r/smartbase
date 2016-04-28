9ecb624b65d1f8c994f76bfba55b0b0d9d66c21d
Alexey Karnov <void.alexey@gmail.com>
2016-04-28 12:18:30 +0300

    Menu component: button action before state change (#1)
    Check if page shown before menu button state change

--- js/components/menu.js
+++ js/components/menu.js
@@ -2,8 +2,20 @@ TVComponents.Menu = function(el, adjacent_buttons, parent, class_name) {
 	TVComponent.call(this, el, adjacent_buttons, parent, class_name);
 };
 TVComponents.Menu.prototype = Object.create(TVComponent.prototype);
+TVComponents.Menu.prototype.onready = function () {
+    for (var btn_id in this.buttons) {
+        var btn = this.buttons[btn_id];
+        btn.onmouseclick = this.onButtonMouseClick.bind(this, btn);
+        btn.el.onclick = btn.onmouseclick;
+    }
+};
 
-TVComponents.Menu.prototype.onButtonClick = function(btn) {	
-	if (this.onclick) this.onclick(btn.id, btn);
-	TV.app.pages[btn.attributes.page_id].show();
+TVComponents.Menu.prototype.onButtonMouseClick = function (btn, event) {
+    var page_to_show = TV.app.pages[btn.attributes.page_id];
+    page_to_show.show();
+    if (app.curr_page.name == page_to_show.name) {
+        TVButton.prototype.onmouseclick.call(btn, event);
+    } else {
+        btn.resetHover();
+    }
 };
